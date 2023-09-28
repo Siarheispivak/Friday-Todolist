41004deff795fde327c32e68044bf166357231e7
Siarheispivak <siarheispivak2@gmail.com>
2023-09-29 01:34:22 +0300

    refactor: transferred the authReducer to slice

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,20 +1,24 @@
-import {tasksReducer} from '../features/TodolistsList/tasks-reducer';
-import {todolistsReducer} from '../features/TodolistsList/todolists-reducer';
-import { AnyAction, applyMiddleware, combineReducers, createStore } from 'redux'
-import thunkMiddleware, { ThunkAction, ThunkDispatch } from 'redux-thunk'
-import {appReducer} from './app-reducer'
-import {authReducer} from '../features/Login/auth-reducer'
+import { tasksReducer } from "features/TodolistsList/tasks-reducer"
+import { todolistsReducer } from "features/TodolistsList/todolists-reducer"
+import { AnyAction, combineReducers } from "redux"
+import { ThunkAction, ThunkDispatch } from "redux-thunk"
+import { appReducer } from "./app-reducer"
+import { authReducer } from "features/Login/auth-reducer"
+import { configureStore } from "@reduxjs/toolkit"
 
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
 const rootReducer = combineReducers({
-    tasks: tasksReducer,
-    todolists: todolistsReducer,
-    app: appReducer,
-    auth: authReducer
+  tasks: tasksReducer,
+  todolists: todolistsReducer,
+  app: appReducer,
+  auth: authReducer,
 })
 // непосредственно создаём store
-export const store = createStore(rootReducer, applyMiddleware(thunkMiddleware));
+export const store = configureStore({
+  reducer: rootReducer,
+})
+
 // определить автоматически тип всего объекта состояния
 export type AppRootStateType = ReturnType<typeof rootReducer>
 
@@ -22,8 +26,3 @@ export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, AppRootStateTy
 
 // export type AppDispatch = typeof store.dispatch
 export type AppDispatch = ThunkDispatch<AppRootStateType, unknown, AnyAction>
-
-
-// а это, чтобы можно было в консоли браузера обращаться к store в любой момент
-// @ts-ignore
-window.store = store;
